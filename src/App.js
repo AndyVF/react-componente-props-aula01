f28631404af55099716c3164e31c011e25236bb6
Anderson <andersonfinho@hotmail.com>
2021-10-13 18:53:00 -0300

    atividade props react

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import React from "react";
+import Carta from "./Carta.js"
+
+function App() {
+  return (
+    <div className="App">
+
+      <Carta nome="Anderson" estado="Pernambuco" />
+      
+    </div>
+  );
+}
+
+export default App;
---
+++ src/Carta.js
@@ -0,0 +1,15 @@
+import React from "react"
+
+export default function Carta(props) {
+ return(
+     <div>
+     <h1>Olá- {props. nome}- {props. estado}</h1>
+     <p>Escrevo essa carta para mim mesmo e digo</p>
+     <p>dependendo das situações e obstaculos e </p>
+     <p>por mais dificil que seja não desista,</p>
+     <p>futuramente estará bem sucedido, se esforce</p>
+     <p>você vai mudar, vai ser melhor, e vai superar</p>
+     <p>seus limites, pois Deus estará contigo.</p>
+     </div>
+ )
+}
